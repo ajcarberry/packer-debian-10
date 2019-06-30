5a316cb2021bb29b6973ca3d992176c0f43314e5
Administrator <alex.carberry@gmail.com>
2019-06-30 01:11:21 -0400

    add dirmngr as pre-req for apt-key

--- scripts/ansible.sh
+++ scripts/ansible.sh
@@ -2,7 +2,7 @@
 
 # Install Ansible repository.
 apt -y update && apt-get -y upgrade
-apt -y install software-properties-common
+apt -y install software-properties-common dirmngr --install-recommends
 apt-add-repository 'deb http://ppa.launchpad.net/ansible/ansible/ubuntu trusty main'
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 93C4A3FD7BB9C367
 
